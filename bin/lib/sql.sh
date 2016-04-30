ffc0bc991d1d8600372f90655853518223e5775e
da99 <i-hate-spam-1234567@mailinator.com>
2016-04-29 22:27:39 -0500

    Added: function sql

---
+++ bin/lib/sql.sh
@@ -0,0 +1,69 @@
+
+# === {{CMD}}  BOTH  path/to/file.sql
+# === {{CMD}}  UP    path/to/file.sql
+# === {{CMD}}  DOWN  path/to/file.sql
+sql () {
+  local +x DIR="$(echo $1 | tr '[:lower:]' '[:upper:]')";  shift
+  local +x FILE="$1"; shift
+
+  case "$DIR" in
+    BOTH|UP|DOWN)
+      :
+      ;;
+    *)
+      mksh_setup RED "!!! Invalid value: {{$DIR}}"
+      exit 1
+      ;;
+  esac
+
+  local +x IFS=$'\n'
+  local +x UP_CONTENT=""
+  local +x DOWN_CONTENT=""
+  local +x NO_NAME=""
+  local +x CURRENT="UP"
+
+  for LINE in $(cat "$FILE"); do
+
+    local +x CLEAN_LINE="$(echo $LINE)"
+    local +x NEW_DIR="$(echo "$CLEAN_LINE" | grep -Po '^[\-\ ]+\K.+' | tr '[:lower:]' '[:upper:]')"
+
+    case "$NEW_DIR" in
+      UP)
+        CURRENT="UP"
+        ;;
+      DOWN)
+        CURRENT="DOWN"
+        ;;
+      BOTH)
+        CURRENT="BOTH"
+        ;;
+      *)
+        case "$CURRENT" in
+          UP)
+            UP_CONTENT="$UP_CONTENT\n$LINE"
+            ;;
+          DOWN)
+            DOWN_CONTENT="$DOWN_CONTENT\n$LINE"
+            ;;
+          BOTH)
+            UP_CONTENT="$UP_CONTENT\n$LINE"
+            DOWN_CONTENT="$DOWN_CONTENT\n$LINE"
+            ;;
+          *)
+            mksh_setup RED "!!! Unknown direction: {{$CURRENT}}"
+            exit 1
+            ;;
+        esac
+        ;;
+    esac
+  done
+
+  if [[ "$DIR" == "UP" ]]; then
+    echo -e "$UP_CONTENT"
+  else
+    echo -e "$DOWN_CONTENT"
+  fi
+} # === end function
+
+
+
