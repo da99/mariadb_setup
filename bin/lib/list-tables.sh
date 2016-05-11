b131a78fe5db0a0cfc7d973dc55b84235199a7d1
da99 <i-hate-spam-1234567@mailinator.com>
2016-05-11 15:14:44 -0500

    Dev. checkpoint: Touchup.

--- bin/lib/list-tables.sh
+++ bin/lib/list-tables.sh
@@ -4,7 +4,10 @@ list-tables () {
   local +x SQL="$(cat <<EOF
   SELECT TABLE_NAME
     FROM INFORMATION_SCHEMA.TABLES
-    WHERE TABLE_SCHEMA = database();
+    WHERE
+    TABLE_SCHEMA = database()
+    AND
+    TABLE_TYPE = 'BASE TABLE';
 EOF)"
   echo "$SQL" | mysql --skip-column-names
 } # === end function
